70adb2d951eed2ce56b822bce360b8de1189ebbf
Dan <dan.nelson8@gmail.com>
2016-11-26 19:46:31 -0600

    Working example Keras network + function to get all ids

--- scripts/make_test_data.py
+++ scripts/make_test_data.py
@@ -10,15 +10,14 @@ using dictionary: truth_dict[doc_id][topic] returns:
 0.5 if the document may be relevant
 1.0 if the document is relevant
 '''
-
 def make_truth(year=2014):
 
     file_name, delimiter = get_file(year)
     print file_name
 
-    # makes a dictionary of dictionaries, with a default value of -1.0
-    truth_by_id = defaultdict(lambda: defaultdict(lambda: -1.0))
-    by_topic = defaultdict(lambda: defaultdict(lambda: -1.0))
+    # makes a dictionary of dictionaries, with a default value of -0.5
+    truth_by_id = defaultdict(lambda: defaultdict(lambda: -0.5))
+    by_topic = defaultdict(lambda: defaultdict(lambda: -0.5))
     known_ids = set()
 
     with open(file_name, 'rb') as f:
@@ -49,9 +48,23 @@ def get_file(year):
         return 'qrels-treceval-2015.txt', ' '
 
 
+# Use this in case we only want to run on the subset of IDs with relevance judgments - this will return said set.
+def get_all_ids():
+    id_set = set()
+    files_delimiters = [get_file(year) for year in [2014, 2015]]
 
-if __name__ == '__main__':
-    truth = make_truth(year=2015)
+    for pair in files_delimiters:
+        with open(pair[0], 'rb') as f:
+            for line in f:
+                parsed_line = [int(val) for val in line.split(pair[1])]
+                id_set.add(parsed_line[2])
+
+    return id_set
+
+
+#if __name__ == '__main__':
+    #truth = make_truth(year=2015)
+    #ids = get_all_ids()
 
 
 
--- scripts/model_training.py
+++ scripts/model_training.py
@@ -1,21 +1,58 @@
 # Model training for DRMM
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from keras.utils.np_utils import to_categorical
+import numpy as np
+import make_test_data
+
 
 # Placeholder data parsing
-with open('histograms', 'r') as f:
-    histograms = [line.split() for line in f]
-X_train = histograms
-Y_train = relevances
+def get_data(training_year=2014):
+    # Proposed format: [doc_id topic_id h0 h1 h2 h3 ...]
+    with open('histograms', 'r') as f:
+        histograms = [line.split() for line in f]
 
-# Create model
+    # can set this to 2015 to read in annotations for 2015 instead
+    # format: label_dict[doc_id][topic_id] = ground truth
+    label_dict = make_test_data.make_truth(training_year)
+
+    X_train = [np.array(hist[2:], int) for hist in histograms]
+    Y_train = np.array([label_dict[int(hist[0])][int(hist[1])] for hist in histograms])
+
+
+def get_fake_data():
+    X_train = np.random.rand(1000, 30)
+
+    # roughly uniform dist of 0, 0.5, 1
+    rand_vals = np.random.rand(1000)
+    Y_train = np.array([np.floor(val * 3)/2.0 for val in rand_vals])
+    return X_train, Y_train
+
+
+# X_train, Y_train = get_data()
+X_train, Y_train = get_fake_data()
+
+# Create model (input_shape is inferred after first layer)
+# This model is a regression
 model = Sequential()
-model.add(Dense(5), input_shape=(30,))
+model.add(Dense(output_dim=30, input_shape=(30,)))
 model.add(Activation('relu'))
-model.add(Dense(1))
+model.add(Dense(output_dim=30))
+model.add(Activation('relu'))
+model.add(Dense(output_dim=1))
 
 # Compile model
 model.compile(loss='mean_squared_error', optimizer='sgd')
 
 # Train model
 model.fit(X_train, Y_train, nb_epoch=5, batch_size=32)
+
+# Test model
+X_test, _ = get_fake_data()
+pred_ranks = model.predict(X_test)
+
+
+
+
+
+
